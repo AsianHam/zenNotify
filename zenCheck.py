5911dd4da0629a3ac37028ff10435d5f7098499e
AsianHam <abeoh1@gmail.com>
2019-08-08 11:55:10 -0400

    Ticket check
    Checks for new tickets
    
    Need to implement infinite loop and delays
    
    Need to implement subject instead of ticket id

--- zenCheck.py
+++ zenCheck.py
@@ -1,30 +1,22 @@
-from lxml import html
-from bs4 import BeautifulSoup
+from zenpy import Zenpy
+from zenpy.lib.api_objects import Ticket
 from win10toast import ToastNotifier
-from requests import Session
-import urllib3
-import re
 
+creds = {
+    'email' : 'spstech@columbia.edu',
+    'password' : '%$MjiKirk106%*',
+    'subdomain' : 'spsservice'
+}
+
+zenpyClient = Zenpy(**creds)
 toaster = ToastNotifier()
 
-http = urllib3.PoolManager()
+newTicket = []
 
-payload = {'username': 'abo2114@columbia.edu', 'pass': 'Abmk-2265'}
-
-login = 'https://spsservice.zendesk.com/hc/en-us/signin?return_to=https%3A%2F%2Fspsservice.zendesk.com%2Fhc%2Fen-us&locale=en-us'
-url = 'https://spsservice.zendesk.com/agent/filters/114115742371'
-
-def readHtml(link):
-    response = http.request('GET', link)
-    bSoup = BeautifulSoup(response.data, 'lxml')
-    return bSoup
-
-with Session() as s:
-    site = s.get(login)
-    bsContent = BeautifulSoup(site.content, "html.parser")
-    loginData = {"username":"abo2114@columbia.edu", 
-                    "password":"Abmk-2265",
-                    "csrf_token":"YHJJ6KDWK1pctlc+mg0dzefVqIXePHJWp1gq6+ZbfNgdJOKY5d3jyQKyCT072sNf"}
-    s.post(login,loginData)
-    homePage = s.get(url)
-    print(homePage.content)
+for ticket in zenpyClient.search(type='ticket', status='new'):
+    if ticket not in newTicket:
+        newTicket.append(ticket)
+        text = "Check ticket: " + str(ticket)
+        toaster.show_toast("New Ticket", text, duration = 15)
+    else:
+        continue
